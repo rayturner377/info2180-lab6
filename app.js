60283df0032db0a56965d65cc21165cac8850bf9
Ray Turner <turnerray377@gmail.com>
2019-11-12 00:36:34 -0500

    Everything Up to alert coming up on screen

---
+++ app.js
@@ -0,0 +1,36 @@
+
+
+window.onload = function(){
+  /*
+  let button = document.getElementById("search");
+
+  button.addEventListener("click",function(){
+    let xmls = new XMLHttpRequest();
+    xmls.onreadystatechange =function(){
+      if(this.readyState === 4)
+      {
+        if(this.status === 200){
+          //var response = httprequest.responseText;
+          alert("well Done");
+        }
+        else{
+          console.log(error);
+        }
+      }
+    }
+    xmls.open('GET', url);
+    xmls.send();
+  })
+  */
+  let button = document.getElementById("search");
+  let bb = document.getElementById('b');
+  button.addEventListener('click',() => {
+    fetch('http://localhost:8080/info2180-lab6/superheroes.php').then(response => response.text())
+    .then(data => {
+      alert(data);
+    }).catch(error => {
+      console.log(error);
+    });
+
+  });
+}
